e2807277a640274e078de9949fc880648679ab8b
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-19 16:35:46 +0800

    added initial code for batch 5 program 5

--- print_correct_casing.py
+++ print_correct_casing.py
@@ -1,6 +1,18 @@
 #ask the user to input their full name in incorrect casing and store it in a variable
+user_input = input("enter your full name in incorrect casing = ")
+
 #split the user string into a list, store in a variable
+string_list = user_input.split()
+
 #use for-loop to iterate through list
-#convert string casing into lower
-#capitalize string
+for noun in string_list:
+
+    #convert string casing into lower
+    noun.lower()
+
+    #capitalize string
+    noun.capitalize()
+
 #use join() to reconvert list into string
+capitalized_result = string_list.join()
+print(capitalized_result)
