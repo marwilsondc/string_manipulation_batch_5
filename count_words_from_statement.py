d8b33569f08ef223fe062aaf019b6298d26a840c
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-19 17:05:49 +0800

    added code for batch 5 program 7

--- count_words_from_statement.py
+++ count_words_from_statement.py
@@ -1,3 +1,8 @@
 #ask user to input a statement with length of desired amount, store in a variable
+user_string = input("input a statement (length of desired amount) = ")
+
 #split the user string into a list, store in a variable 
+string_list = user_string.split()
+
 #utilize count() to return number of words in user string
+print(string_list.count())
