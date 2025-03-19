7cf68409053840d191e2467a2f191e7448fa0613
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-19 16:28:02 +0800

    added initial pseudocode for batch 5 program 5

---
+++ print_correct_casing.py
@@ -0,0 +1,6 @@
+#ask the user to input their full name in incorrect casing and store it in a variable
+#split the user string into a list, store in a variable
+#use for-loop to iterate through list
+#convert string casing into lower
+#capitalize string
+#use join() to reconvert list into string
