b4cbbbfd15cdb65235ae2055f0ba8d0775aded9b
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-19 16:44:18 +0800

    fixed bugs for batch 5 program 5

--- print_correct_casing.py
+++ print_correct_casing.py
@@ -1,3 +1,6 @@
+#initialize capitalized_list
+capitalized_list = list()
+
 #ask the user to input their full name in incorrect casing and store it in a variable
 user_input = input("enter your full name in incorrect casing = ")
 
@@ -8,11 +11,14 @@ string_list = user_input.split()
 for noun in string_list:
 
     #convert string casing into lower
-    noun.lower()
+    step_one = noun.lower()
 
     #capitalize string
-    noun.capitalize()
+    step_two = step_one.capitalize()
+
+    #append capitalized nouns to capitalized_list
+    capitalized_list.append(step_two)
 
 #use join() to reconvert list into string
-capitalized_result = string_list.join()
+capitalized_result = " ".join(capitalized_list)
 print(capitalized_result)
