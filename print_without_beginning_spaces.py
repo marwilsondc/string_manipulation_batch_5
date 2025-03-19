943bc3decca3ab3067cc03ebfe433de6e9228574
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-19 15:45:33 +0800

    added the initial code and fixed bug for batch 5 program 1

--- print_without_beginning_spaces.py
+++ print_without_beginning_spaces.py
@@ -1,2 +1,5 @@
 #ask the user to input a string with several spaces before the text itself
+user_string = input("input a string with however much spaces you want before the text = ")
+
 #print the user input without the spaces put before the text
+print(user_string.lstrip())
