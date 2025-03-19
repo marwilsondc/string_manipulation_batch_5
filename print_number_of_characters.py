cda8add977de8747904e7150f1b020f2bbfe88d6
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-19 17:21:49 +0800

    added pseudocode for batch 5 program 8

---
+++ print_number_of_characters.py
@@ -0,0 +1,6 @@
+#ask user to input their full name 
+#strip the user string of whitespaces
+#initialize count
+#use for loop to iterate through string characters
+#add 1 to count for each iteration
+#return value of count
