2ed37da604bdbbab5965c70f7d3af971315d5aa8
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-19 17:57:54 +0800

    added code for batch 5 program 10

--- print_snake_case.py
+++ print_snake_case.py
@@ -1,3 +1,11 @@
 #ask user to enter their full name in incorrect casing
+user_string = input("input your name in incorrect casing = ")
+
 #convert user string to lower case
+convert_lower = user_string.lower()
+
 #convert whitespaces to underscore
+final_result = convert_lower.replace(" ", "_")
+
+#print result
+print(final_result)
