7c12defedad52bd8742c85758441cb05c191fe3d
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-19 17:03:03 +0800

    added pseudocode for batch 5 program 7

---
+++ count_words_from_statement.py
@@ -0,0 +1,3 @@
+#ask user to input a statement with length of desired amount, store in a variable
+#split the user string into a list, store in a variable 
+#utilize count() to return number of words in user string
