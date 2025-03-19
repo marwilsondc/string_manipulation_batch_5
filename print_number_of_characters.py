5b76189c605857affe25033eebc708ea6aff784f
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-19 17:26:14 +0800

    added initial code for batch 5 program 8

--- print_number_of_characters.py
+++ print_number_of_characters.py
@@ -1,6 +1,18 @@
 #ask user to input their full name 
-#strip the user string of whitespaces
+user_string = input("input your full name = ")
+
+#strip the user string of whitespaces, store in a variables
+stripped_string = user_string.strip()
+
 #initialize count
+count = 0 
+
+print(stripped_string)
 #use for loop to iterate through string characters
-#add 1 to count for each iteration
+for i in stripped_string:
+    
+    #add 1 to count for each iteration
+    count += 1
+
 #return value of count
+print(count)
