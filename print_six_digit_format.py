9d675af5b2ff3a6ebfd919b8d9503b54dea136f4
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-03-19 16:02:53 +0800

    added the code for batch 5 program 2

--- print_six_digit_format.py
+++ print_six_digit_format.py
@@ -1,2 +1,5 @@
 #ask the user to input a random number (0-1000) store it in a variable
+user_input = input("input a number between 0-1000 = ")
+
 #print the six digit format with zfill()
+print(user_input.zfill(6))
